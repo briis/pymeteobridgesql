b92054a1e0704ccf5b6a7828b4d27cf02c226fa5
Bjarne Riis <bjarne@briis.com>
2024-02-10 17:12:42 +0100

    Bump to 1.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="pymeteobridgesql",
-    version="1.1.1",
+    version="1.1.2",
     author="briis",
     author_email="bjarne@briis.com",
     description="Gets weather data from a MySQL table",
