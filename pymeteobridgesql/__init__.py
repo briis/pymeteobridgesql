994265e6593b0db16151caf84b131cec6b34a241
Bjarne Riis <bjarne@briis.com>
2024-04-29 15:16:33 +0200

    Bump to 1.2.1

--- pymeteobridgesql/__init__.py
+++ pymeteobridgesql/__init__.py
@@ -4,9 +4,9 @@
 from __future__ import annotations
 
 from .api import MeteobridgeSQL, MeteobridgeSQLDatabaseConnectionError, MeteobridgeSQLDataError
-from .data import ForecastDaily, ForecastHourly, RealtimeData, StationData
+from .data import ForecastDaily, ForecastHourly, MinuteData, MonthlyData, RealtimeData, StationData
 
 __title__ = "pymeteobridgesql"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "briis"
 __license__ = "MIT"
